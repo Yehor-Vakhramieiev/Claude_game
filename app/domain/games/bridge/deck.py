b6501938a5de4ca9802d95ab28f29137fb2986a7
yehorvakhramieiev <vahrameev.egor.00@gmail.com>
2026-03-21 14:59:50 +0000

    Base deck entity

---
+++ app/domain/games/bridge/deck.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+
+from cards import Card, CardSuit, CardRank, CARD_REGISTRY
+
+
+class Deck(BaseModel):
+    draw_pile: list[Card] = Field(default_factory=list)
+    discard_pile: list[Card] = Field(default_factory=list)
+    flips_count: int = 0
+
+    @classmethod
+    def create_standard_36(cls) -> "Deck":
+        pile = []
+
+        for suit in CardSuit:
+            for rank in CardRank:
+                card_class = (
+                    CARD_REGISTRY.get((rank, suit))
+                    or CARD_REGISTRY.get((rank, None))
+                    or Card
+                )
+                pile.append(card_class(suit=suit, rank=rank))
+
+        deck = cls(draw_pile=pile)
+        deck.shuffle()
+        return deck
+
+    def shuffle(self): ...
