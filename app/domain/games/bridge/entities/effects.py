71e95232febaad1ba67d2789842a450c2dfc639a
yehorvakhramieiev <vahrameev.egor.00@gmail.com>
2026-03-22 19:14:47 +0000

    fix future serialisation bag

--- app/domain/games/bridge/entities/effects.py
+++ app/domain/games/bridge/entities/effects.py
@@ -1,20 +1,23 @@
+from typing import Literal
 from pydantic import BaseModel, Field
 
 
 class DrawEffect(BaseModel):
+    type: Literal["draw"] = "draw"
     amount: int = Field(ge=1, le=5)
 
 
 class SkipEffect(BaseModel):
+    type: Literal["skip"] = "skip"
     amount: int = 1
 
 
 class ChangeSuitEffect(BaseModel):
-    pass
+    type: Literal["change_suit"] = "change_suit"
 
 
 class CoverEffect(BaseModel):
-    pass
+    type: Literal["cover"] = "cover"
 
 
 Effect = DrawEffect | SkipEffect | ChangeSuitEffect | CoverEffect
