c2228e695abfee22cb197b3515c97cae58309088
yehorvakhramieiev <vahrameev.egor.00@gmail.com>
2026-03-22 18:16:01 +0000

    deck first version of entity

--- app/domain/games/bridge/entities/deck.py
+++ app/domain/games/bridge/entities/deck.py
@@ -1,3 +1,5 @@
+import random
+
 from pydantic import BaseModel, Field
 
 from app.domain.games.bridge.entities.cards import (
@@ -8,26 +10,31 @@ from app.domain.games.bridge.entities.cards import (
 )
 
 
+def create_standard_36() -> list[Card]:
+    pile = []
+
+    for suit in CardSuit:
+        for rank in CardRank:
+            card_class = (
+                CARD_REGISTRY.get((rank, suit))
+                or CARD_REGISTRY.get((rank, None))
+                or Card
+            )
+            pile.append(card_class(suit=suit, rank=rank))
+    return pile
+
+
 class Deck(BaseModel):
-    draw_pile: list[Card] = Field(default_factory=list)
+    draw_pile: list[Card] = Field(default_factory=create_standard_36)
     discard_pile: list[Card] = Field(default_factory=list)
     flips_count: int = 0
 
-    @classmethod
-    def create_standard_36(cls) -> "Deck":
-        pile = []
+    def shuffle(self, amount_to_save: int = 0):
+        if len(self.discard_pile) == amount_to_save:
+            random.shuffle(self.draw_pile)
+            return
 
-        for suit in CardSuit:
-            for rank in CardRank:
-                card_class = (
-                    CARD_REGISTRY.get((rank, suit))
-                    or CARD_REGISTRY.get((rank, None))
-                    or Card
-                )
-                pile.append(card_class(suit=suit, rank=rank))
-
-        deck = cls(draw_pile=pile)
-        deck.shuffle()
-        return deck
-
-    def shuffle(self): ...
+        self.draw_pile.extend(self.discard_pile[:-amount_to_save])
+        self.discard_pile = self.discard_pile[-amount_to_save:]
+        self.flips_count += 1
+        self.shuffle()
