107f2116240b1ae1d9aef579b125e9c50fcf2fc6
yehorvakhramieiev <vahrameev.egor.00@gmail.com>
2026-03-23 19:18:55 +0000

    add all entities to __init__.py for easier import

---
+++ app/domain/games/bridge/entities/__init__.py
@@ -0,0 +1,13 @@
+from .deck import Deck
+from .cards import Card
+from .effects import Effect, SkipEffect, CoverEffect, DrawEffect, ChangeSuitEffect
+
+__all__ = (
+    "Deck",
+    "Card",
+    "Effect",
+    "SkipEffect",
+    "CoverEffect",
+    "DrawEffect",
+    "ChangeSuitEffect",
+)
