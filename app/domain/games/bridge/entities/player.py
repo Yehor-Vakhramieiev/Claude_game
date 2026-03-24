7e4fbefce71eb6b15d5c06650673a12488fe519e
yehorvakhramieiev <vahrameev.egor.00@gmail.com>
2026-03-24 20:07:25 +0000

    methods to add and removw cards

--- app/domain/games/bridge/entities/player.py
+++ app/domain/games/bridge/entities/player.py
@@ -21,3 +21,21 @@ class Player(BaseModel):
     @classmethod
     def restore_hand(cls, data: Any) -> list[Card]:
         return restore_from_data(data)
+
+    def add_cards(self, data: list[Card] | Card) -> None:
+        cards = [data] if isinstance(data, Card) else data
+        for card in cards:
+            if card in self.hand:
+                raise CardAlreadyInHandError(f"Card {card} is already in the hand")
+
+        self.hand.extend(cards)
+
+    def remove_cards(self, data: list[Card] | Card) -> None:
+        cards = [data] if isinstance(data, Card) else data
+
+        for card in cards:
+            if cards.count(card) > self.hand.count(card):
+                raise CardNotRemovedError(f"Card {card} not removed")
+
+        for card in cards:
+            self.hand.remove(card)
